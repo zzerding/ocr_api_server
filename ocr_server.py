8f907bc3bd75890b1d99ac921fa2b019796ce74f
zzerd <zzerding@gmail.com>
2023-03-22 22:52:08 +0800

    Update ocr_server.py
    加入验证码识别脚本

--- ocr_server.py
+++ ocr_server.py
@@ -2,9 +2,11 @@
 import argparse
 import base64
 import json
+import sqlite3
 
 import ddddocr
-from flask import Flask, request
+from flask import Flask, request,g
+import hashlib
 
 parser = argparse.ArgumentParser(description="使用ddddocr搭建的最简api服务")
 parser.add_argument("-p", "--port", type=int, default=9898)
@@ -16,111 +18,92 @@ args = parser.parse_args()
 
 app = Flask(__name__)
 
+DATABASE = 'data.db'
 
-class Server(object):
-    def __init__(self, ocr=True, det=False, old=False):
-        self.ocr_option = ocr
-        self.det_option = det
-        self.old_option = old
-        self.ocr = None
-        self.det = None
-        if self.ocr_option:
-            print("ocr模块开启")
-            if self.old_option:
-                print("使用OCR旧模型启动")
-                self.ocr = ddddocr.DdddOcr(old=True)
+def init_db():
+    with app.app_context():
+        db = get_db()
+        cursor = db.cursor()
+# Execute the SQL command to modify the table
+        cursor.execute('''CREATE TABLE IF NOT EXISTS rule (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            host TEXT,
+                            path TEXT,
+                            input TEXT,
+                            img TEXT,
+                            title TEXT,
+                            type TEXT,
+                            idcard TEXT,
+                            code INTEGER
+                            )''')
+
+        cursor.execute('''CREATE INDEX IF NOT EXISTS index_path ON rule (path)''')
+# Commit the changes to the database
+        db.commit()
+
+# 重写row_factory方法，返回字典
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = sqlite3.connect(DATABASE)
+        db.row_factory = dict_factory
+        g._database = db
+# execute a query
+        cursor.execute('SELECT * FROM rule WHERE path = ?',[path])
+        rows = cursor.fetchall()
+        app.logger.info(rows)
+        if rows:
+            return json.dumps({"code":531,"data":rows})
+        else:
+            return json.dumps({"code":533,"info": "未找到规则"})
+    except Exception as e:
+        return set_ret(e, "json")
+@app.route('/captchaHostAdd',methods=['POST'])
+def addOcr():
+    try:
+        rsp = request.get_json()
+        path = get_path_md5(rsp['path'])
+        app.logger.info(path)
+        db = get_db()
+        cursor = db.cursor()
+# execute a query
+        cursor.execute('SELECT * FROM rule WHERE path = ?',[path])
+        data = cursor.fetchone()
+        app.logger.info(data)
+        app.logger.info('-------')
+        if  data is None:
+#add rsp to sqlite
+            rsp['code'] = 531
+            rsp['path'] = path
+            columns = ', '.join(rsp.keys())
+            placeholders = ', '.join('?' * len(rsp))
+            sql = 'INSERT INTO rule ({}) VALUES ({})'.format(columns, placeholders)
+            values = [int(x) if isinstance(x, bool) else x for x in rsp.values()]
+            app.logger.info(sql)
+            app.logger.info(values)
+            cursor.execute(sql, values)
+# 获取受影响的行数
+            rowcount = cursor.rowcount
+# 判断是否插入成功
+            if rowcount > 0:
+                app.logger.info("插入成功")
             else:
-                print("使用OCR新模型启动，如需要使用旧模型，请额外添加参数  --old开启")
-                self.ocr = ddddocr.DdddOcr()
-        else:
-            print("ocr模块未开启，如需要使用，请使用参数  --ocr开启")
-        if self.det_option:
-            print("目标检测模块开启")
-            self.det = ddddocr.DdddOcr(det=True)
-        else:
-            print("目标检测模块未开启，如需要使用，请使用参数  --det开启")
-
-    def classification(self, img: bytes):
-        if self.ocr_option:
-            return self.ocr.classification(img)
-        else:
-            raise Exception("ocr模块未开启")
-
-    def detection(self, img: bytes):
-        if self.det_option:
-            return self.det.detection(img)
-        else:
-            raise Exception("目标检测模块模块未开启")
-
-    def slide(self, target_img: bytes, bg_img: bytes, algo_type: str):
-        dddd = self.ocr or self.det or ddddocr.DdddOcr(ocr=False)
-        if algo_type == 'match':
-            return dddd.slide_match(target_img, bg_img)
-        elif algo_type == 'compare':
-            return dddd.slide_comparison(target_img, bg_img)
-        else:
-            raise Exception(f"不支持的滑块算法类型: {algo_type}")
-
-server = Server(ocr=args.ocr, det=args.det, old=args.old)
-
-
-def get_img(request, img_type='file', img_name='image'):
-    if img_type == 'b64':
-        img = base64.b64decode(request.get_data()) # 
-        try: # json str of multiple images
-            dic = json.loads(img)
-            img = base64.b64decode(dic.get(img_name).encode())
-        except Exception as e: # just base64 of single image
-            pass
-    else:
-        img = request.files.get(img_name).read()
-    return img
-
-
-def set_ret(result, ret_type='text'):
-    if ret_type == 'json':
-        if isinstance(result, Exception):
-            return json.dumps({"status": 200, "result": "", "msg": str(result)})
-        else:
-            return json.dumps({"status": 200, "result": result, "msg": ""})
-        # return json.dumps({"succ": isinstance(result, str), "result": str(result)})
-    else:
-        if isinstance(result, Exception):
-            return ''
-        else:
-            return str(result).strip()
-
-
-@app.route('/<opt>/<img_type>', methods=['POST'])
-@app.route('/<opt>/<img_type>/<ret_type>', methods=['POST'])
-def ocr(opt, img_type='file', ret_type='text'):
-    try:
-        img = get_img(request, img_type)
-        if opt == 'ocr':
-            result = server.classification(img)
-        elif opt == 'det':
-            result = server.detection(img)
-        else:
-            raise f"<opt={opt}> is invalid"
-        return set_ret(result, ret_type)
+                app.logger.info("插入失败")
+#提交事务
+            db.commit()
+            app.logger.info('-------')
+            return json.dumps({"code": 200})
+        return set_ret("not in", "json")
     except Exception as e:
-        return set_ret(e, ret_type)
-
-@app.route('/slide/<algo_type>/<img_type>', methods=['POST'])
-@app.route('/slide/<algo_type>/<img_type>/<ret_type>', methods=['POST'])
-def slide(algo_type='compare', img_type='file', ret_type='text'):
-    try:
-        target_img = get_img(request, img_type, 'target_img')
-        bg_img = get_img(request, img_type, 'bg_img')
-        result = server.slide(target_img, bg_img, algo_type)
-        return set_ret(result, ret_type)
-    except Exception as e:
-        return set_ret(e, ret_type)
-
-@app.route('/ping', methods=['GET'])
-def ping():
-    return "pong"
+        return set_ret(e, "json")
 
 
 if __name__ == '__main__':
+    init_db()
     app.run(host="0.0.0.0", port=args.port)
